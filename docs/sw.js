895af8a6627d5491979a72028674b7c4c037ca0e
charmedimsure <als.aldridge@gmail.com>
2026-07-22 17:35:50 -0400

    call signaling out of history, ring only the called person

--- docs/sw.js
+++ docs/sw.js
@@ -7,7 +7,7 @@
    as the tablet has signal. The cache exists only so the app still
    opens when the wifi is down. */
 
-const VERSION = '2026-07-22-e';
+const VERSION = '2026-07-22-f';
 const CACHE   = 'independentme-' + VERSION;
 const SHELL   = ['./', './index.html', './care.html', './manifest.json', './manifest-care.json'];
 
